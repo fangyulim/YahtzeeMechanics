50404aac0baa4dbd2de203c37ecfdf5675dd7f63
jingrentay <75294705+jingrentay@users.noreply.github.com>
2020-12-08 15:41:59 -0800

    Add files via upload

--- graphics.h
+++ graphics.h
@@ -5,20 +5,22 @@
 #include <SFML/Window.hpp>
 //#include "scoreboard.h"
 #include <time.h>
+#include <sstream>
+using namespace std;
 
 class Graphics : public Yahtzee
 {
 private:
-	
+
 public:
 
 	void run();
 	void C1draw();
 	void C2draw();
 	void C3draw();
-	
+
 	Graphics()
 	{
-		
+
 	}
 };
