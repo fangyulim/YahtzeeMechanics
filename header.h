e79e1430ef72fe3e33176a2c05b7c0b35ff09a52
fangyulim <70162934+fangyulim@users.noreply.github.com>
2020-12-07 21:21:24 -0800

    Add files via upload

---
+++ header.h
@@ -0,0 +1,36 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+class Yahtzee
+{
+private:
+	int sumOnes;
+	int sumTwos;
+	int sumThrees;
+	int sumFours;
+	int sumFives;
+	int sumSixs;
+	int threeKind;
+	int fourKind;
+	int fullHouse;
+	int smallSt;
+	int largeSt;
+	int yahtzeeS;
+	int chance;
+	int totalS;
+
+public:
+
+	Yahtzee()
+	{
+
+	}
+
+};
+
+#endif
