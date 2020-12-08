f04b7b7127e2240e3ef8f9e66eaa6e034ceddc9f
fangyulim <70162934+fangyulim@users.noreply.github.com>
2020-12-07 23:14:49 -0800

    Update header.h

--- header.h
+++ header.h
@@ -1,62 +1,100 @@
 #ifndef HEADER_H
 #define HEADER_H
 #include <iostream>
-#include <SFML\Graphics.hpp>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
 
-#define RULES 1
-#define PLAY 2
-#define EXIT 3
-
-using namespace std;
-
 class Yahtzee
 {
 private:
-	int option;
-	int any_array;
-	int size;
-	int scorecard;
-	int player;
-	int score_selection;
-	int dice;
-	int num;
-	int die_held;
-	int die;
-	int dice_value_count;
-	int dice_value;
-	int num_of_value;
-	int kind;
-	int yes_no;
+	int p2sumOnes;
+	int p2sumTwos;
+	int p2sumThrees;
+	int p2sumFours;
+	int p2sumFives;
+	int p2sumSixs;
+	int p2threeOKind;
+	int p2fourOKind;
+	int p2fullHouse;
+	int p2smallSt;
+	int p2largeSt;
+	int p2yahtzeeS;
+	int p2chance;
+	int p2totalS;
+	int p2bonus;
+
+	//int option;
+	//int any_array;
+	//int size;
+	//int scorecard;
+	//int player;
+	//int score_selection;
+	int p2dice[6];
+	int p2num;
+	int p2die_held;
+	int p2die[6];
+	int p2diceValueCount[6];
+	int p2diceValue;
+	int p2numValue;
+	//int kind;
+	//int yes_no;
 
 public:
 
-	int menu(void);
-	void run_app(int);
 
-	void reset(int any_array[], int size);
+	Yahtzee()
+	{
+		int p2sumOnes = 0;
+		int p2sumTwos = 0;
+		int p2sumThrees = 0;
+		int p2sumFours = 0;
+		int p2sumFives = 0;
+		int p2sumSixs = 0;
+		int p2threeOKind = 0;
+		int p2fourOKind = 0;
+		int p2fullHouse = 0;
+		int p2smallSt = 0;
+		int p2largeSt = 0;
+		int p2yahtzeeS = 0;
+		int p2chance = 0;
+		int p2totalS = 0;
+		int p2bonus = 0;
 
-	void print_game_rules(void);
-	void play_game(int scorecard[], int player, int score_selection[]);
-	void roll_dice(int dice[], int size);
-	void count_dice(int dice[], int size, int dice_value_count[]);
-	void print_dice(int num[], int size);
-	void hold_die(int die_held[], int die[]);
+		int p2dice=0;
+		int p2num=0;
+		int p2noDieHold=0;
+		int p2die=0;
+		int p2diceValueCount=0;
+		int p2diceValue=0;
+		int p2numValue=0;
+	}
+	
 
-	void combinations(int scorecard[], int dice[], int dice_value_count[], int player, int score_selection[]);
-	int combo_menu(void);
-	int sums(int dice_value, int num_of_value);
-	int sum_all_die(int die[], int size);
-	int of_a_kind(int dice_value_count[], int size, int kind);
-	int is_full_house(int dice_value_count[], int size);
-	int is_small_straight(int dice_value_count[], int size);
-	int is_large_straight(int dice_value_count[], int size);
-	int is_yahtzee(int dice_value_count[], int size);
+	void rollDice(int dice[]);
+	void countDice(int dice[], int dice_value_count[]);
+	//int calcFullH();
+	void holdDie(int p2die_held[],int die[]);
+	int sumOne();
+	int sumTwo();
+	int sumThree();
+	int sumFour();
+	int sumFive();
+	int sumSix();
+	int BonusCal();
+	int threeKind();
+	int fourKind();
+	int full_House();
+	int smallStraight();
+	int largeStraight();
+	int Yahtzeee();
+	void invalid();
+	int totalScore();
+	void reset(int anyArray[], int size);
 
-	void invalid_selection(int yes_no, int scorecard[], int dice[], int dice_value_count[], int player, int score_selection[], int option);
+	void playGame();
+
 
 
 };
